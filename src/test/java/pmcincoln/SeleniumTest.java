be843d299185485913264f86f219e0aa3697dc64
lgutro <38503093+lgutro@users.noreply.github.com>
2018-12-03 07:21:19 -0500

    Put files one directory above

---
+++ src/test/java/pmcincoln/SeleniumTest.java
@@ -0,0 +1,15 @@
+package pmcincoln;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		
+		System.out.println("Hello World\n");
+		
+	}
+
+}
